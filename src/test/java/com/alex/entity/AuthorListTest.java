d5ada06e67e167b0c8bf66761b53c4de2db3bb78
AlexanderKodor <informghp3@gmail.com>
2024-05-20 16:59:18 +0300

    AuthorListTest добавил, но не написал

--- src/test/java/com/alex/entity/AuthorListTest.java
+++ src/test/java/com/alex/entity/AuthorListTest.java
@@ -1,4 +1,19 @@
 package com.alex.entity;
 
+import com.alex.service.StringToFio;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class AuthorListTest {
+    @Mock
+    private StringToFio stringToFio;
+
+    @Test
+    void addAuthor_access(){
+
+    }
+
 }
