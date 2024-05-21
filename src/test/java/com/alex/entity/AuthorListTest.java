8db8cb7e7a3d2781223f7a723a5e5411b3e74904
AlexanderKodor <informghp3@gmail.com>
2024-05-21 11:57:26 +0300

    AuthorListTest добавил, написал, но в голове ещё не уложилось

--- src/test/java/com/alex/entity/AuthorListTest.java
+++ src/test/java/com/alex/entity/AuthorListTest.java
@@ -3,17 +3,25 @@ package com.alex.entity;
 import com.alex.service.StringToFio;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthorListTest {
     @Mock
     private StringToFio stringToFio;
 
+    @InjectMocks
+    private AuthorList authorList;
+
     @Test
     void addAuthor_access(){
-
+        String strName = "Сидоров Петр Иванович";
+        FullName fullName = new FullName("Петр", "Иванович", "Сидоров");
+        doReturn(fullName).when(authorList).addAuthor(strName);
+//        doReturn(fullName).when(authorList).addAuthor(strName);
     }
 
 }
