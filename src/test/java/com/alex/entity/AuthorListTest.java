770efd292161c0719300efbe74b72bee4bd1177e
AlexanderKodor <informghp3@gmail.com>
2024-05-21 12:00:46 +0300

    AuthorListTest добавил тесты access и failed, написал, но в голове ещё не уложилось

--- src/test/java/com/alex/entity/AuthorListTest.java
+++ src/test/java/com/alex/entity/AuthorListTest.java
@@ -1,5 +1,6 @@
 package com.alex.entity;
 
+import com.alex.exception.RegexValidationException;
 import com.alex.service.StringToFio;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,6 +22,14 @@ public class AuthorListTest {
         String strName = "Сидоров Петр Иванович";
         FullName fullName = new FullName("Петр", "Иванович", "Сидоров");
         doReturn(fullName).when(authorList).addAuthor(strName);
+//        doReturn(fullName).when(authorList).addAuthor(strName);
+    }
+
+    @Test
+    void addAuthor_failed(){
+        String strName = "Сидоров Петр И";
+        FullName fullName = new FullName("Петр", "Иванович", "Сидоров");
+        doThrow(RegexValidationException.class).when(authorList).addAuthor(strName);
 //        doReturn(fullName).when(authorList).addAuthor(strName);
     }
 
